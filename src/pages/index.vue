13507bc1e4610f44f0793d7e78a1cf0e07b5e394
Ilya M. <wuvuxd@proton.me>
2024-10-01 08:16:29 +0700

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+defineOptions({
+  name: 'IndexPage',
+})
+
+const name = ref('')
+
+const router = useRouter()
+function go() {
+  if (name.value)
+    router.push(`/hi/${encodeURIComponent(name.value)}`)
+}
+</script>
+
+<template>
+  <div>
+    Main
+  </div>
+</template>
---
+++ uno.config.ts
@@ -0,0 +1,22 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  presetWebFonts,
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+      warn: true,
+    }),
+    presetWebFonts({
+      fonts: {},
+    }),
+  ],
+})
